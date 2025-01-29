c468f8decfcd321451d757652ad77073c2fd93cc
Eduardo Barroso <dev@edbar.xyz>
2025-01-29 15:47:22 -0300

    chore(app): add database migrations
    Add up and down migration for the MOVIES table with its additional constraints that reflect our business rules.

---
+++ migrations/000002_add_movies_check_constraints.down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE tableName DROP CONSTRAINT IF EXISTS movies_runtime_check;
+
+ALTER TABLE tableName DROP CONSTRAINT IF EXISTS movies_year_check;
+
+ALTER TABLE tableName DROP CONSTRAINT IF EXISTS genres_length_check;
+
